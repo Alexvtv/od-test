95558e34c0cf7fa49c11fa524796b8892655b406
Alexandr <63146220+Alexvtv@users.noreply.github.com>
2021-12-02 10:09:58 +0300

    fix result component

--- src/app/components/popup/parts/result/Result.js
+++ src/app/components/popup/parts/result/Result.js
@@ -3,19 +3,14 @@ import {endingResolver} from './utils';
 
 import styles from './result.module.scss';
 
-export const Result = ({data}) => {
-
-    return (
-        <div className={styles.result}>
-            <p className={styles.subtitle}>Итого можете внести в качестве досрочных:</p>
-            {data.map((amount, index) => (
-                <div key={index} className={styles.annual}>
-                    <input type='checkbox'/>
-                    <p>{amount.toLocaleString('ru')} рублей
-                        <span> {index === 1 ? 'во' : 'в'} {index + 1}-{endingResolver(index + 1)} год</span>
-                    </p>
-                </div>
-            ))}
+export const Result = ({data}) => <div className={styles.result}>
+    <p className={styles.subtitle}>Итого можете внести в качестве досрочных:</p>
+    {data.map((amount, index) => (
+        <div key={index} className={styles.annual}>
+            <input type='checkbox'/>
+            <p>{amount.toLocaleString('ru')} рублей
+                <span> {index === 1 ? 'во' : 'в'} {index + 1}-{endingResolver(index + 1)} год</span>
+            </p>
         </div>
-    );
-};
+    ))}
+</div>;
