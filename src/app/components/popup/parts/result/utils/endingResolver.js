eeb23697e4467c63ee7d3c4a7fd2170a87c4609e
Alexandr <63146220+Alexvtv@users.noreply.github.com>
2022-03-05 12:57:40 +0300

    remove element

--- src/app/components/popup/parts/result/utils/endingResolver.js
+++ src/app/components/popup/parts/result/utils/endingResolver.js
@@ -1,4 +1,4 @@
-export const endingResolver = (num) => {;
+export const endingResolver = (num) => {
     switch (true) {
         case ([2, 6, 7, 8].includes(num % 10) && (num < 10 || num > 20)):
             return 'ой';
